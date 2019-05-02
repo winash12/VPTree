446b920a2e2162065d927a933f8f4f597943e114
Ashwin <winash12@gmail.com>
2019-05-02 08:16:18 +0530

    24th commit

--- Distance.cpp
+++ Distance.cpp
@@ -10,7 +10,7 @@ double Distance::calculateDistance(shared_ptr<Point> point1, shared_ptr<Point> p
 
 double GreatCircleDistance::calculateDistance(SphericalPoint point1,SphericalPoint point2)
 {
-  double lat = point1.getCoordinate1();
+  double lat = point2.getCoordinate1();
   double lon = point2.getCoordinate2();
   geod.Inverse(point1.getCoordinate1(),point1.getCoordinate2(),lat,lon,distance);
   return distance;
