308423ec94a0417aa3ddb4efd5296fbb3be7796e
Ashwin <winash12@gmail.com>
2019-05-17 15:42:45 +0530

    40th commit

--- Distance.cpp
+++ Distance.cpp
@@ -11,9 +11,9 @@ double Distance::calculateDistance(shared_ptr<Point> point1, shared_ptr<Point> p
 
 double GreatCircleDistance::calculateDistance(shared_ptr<Point> point1,shared_ptr<Point> point2)
 {
-  double lat = point1.getCoordinate1();
-  double lon = point2.getCoordinate2();
-  geod.Inverse(point1.getCoordinate1(),point1.getCoordinate2(),lat,lon,distance);
+  double lat = point1->getCoordinate1();
+  double lon = point2->getCoordinate2();
+  geod.Inverse(point1->getCoordinate1(),point1->getCoordinate2(),lat,lon,distance);
   if (!(distance >= 0))
     throw GeographicErr("distance does not satisfy d >= 0");
   return distance;
