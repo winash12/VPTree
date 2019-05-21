e91d617cc8d08824b97826fbdef2e0d02cd80eae
Ashwin <winash12@gmail.com>
2019-05-21 08:54:10 +0530

    48th commit

--- Distance.cpp
+++ Distance.cpp
@@ -13,16 +13,15 @@ double GreatCircleDistance::calculateDistance(shared_ptr<Point> point1,shared_pt
 {
   double lat2 = point2->getCoordinate1();
   double lon2 = point2->getCoordinate2();
-  cout << "The value of lat is " << lat2 << endl;
-  cout << "The value of lon is " << lon2 << endl;
+  //cout << "The value of lat is " << lat2 << endl;
+  //cout << "The value of lon is " << lon2 << endl;
   double lat1 = point1->getCoordinate1();
   double lon1 =  point1->getCoordinate2();
-  cout << "The value of lat is " << lat1 << endl;
-  cout << "The value of lon is " << lon1 << endl;
+  //cout << "The value of lat is " << lat1 << endl;
+  //cout << "The value of lon is " << lon1 << endl;
 
   geod.Inverse(lat1,lon1,lat2,lon2,distance);
-  cout << "The value of distance is " << distance << endl;
-  exit(0);
+  //cout << "The value of distance is " << distance << endl;
   if (!(distance >= 0))
     throw GeographicErr("distance does not satisfy d >= 0");
   return distance;
