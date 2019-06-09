db4d3c47b7725812a33ccd94f2601706e9a2cc4a
Ashwin <winash12@gmail.com>
2019-06-09 11:17:07 +0530

    85th commit

--- vptree.pyx
+++ vptree.pyx
@@ -9,13 +9,11 @@ cimport cython
 
 import psutil
 from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 
 from libc.stdio cimport printf
-from libcpp.memory cimport shared_ptr,unique_ptr,make_shared,make_unique,dynamic_pointer_cast
-from libcpp.vector cimport vector
 from libcpp.deque cimport deque
 from libcpp.pair cimport pair
-from libcpp.list cimport list
 
 from cython.operator cimport dereference
 from cython.operator cimport postincrement as inc
@@ -31,12 +29,12 @@ cdef class PyVPTree:
     cdef VPTree *vptree
     cdef deque[Point] points
     cdef Distance *gcd
-    cdef int number_of_processors
+
 
     def __cinit__(self):
         self.vptree = new VPTree()
         self.points = deque[Point]()
-        self.number_of_processors = cpu_count()
+
     def initializeGreatCircleDistance(self):
 
         self.gcd = new GreatCircleDistance()
@@ -62,20 +60,21 @@ cdef class PyVPTree:
         chunk = np.array_split(gridPoints,number_of_processors,axis=0)
         x = ThreadPoolExecutor(max_workers=number_of_processors) 
         t0 = time.time()
-        args = ((chunk,maxDistance))
-        results = x.map(self.getNeighborsInRangeChunk, args)
+        func = partial(self.getNeighborsInRangeChunk,maxDistance)
+        results = x.map(func,chunk)
         t1 = time.time()
         print(t1-t0)
         return results
 
-    def getNeighborsInRangeChunk(self,np.ndarray[np.float64_t,ndim=2] gridPoints,np.float64_t maxDistance):
+    def getNeighborsInRangeChunk(self,np.float64_t maxDistance,np.ndarray[np.float64_t,ndim=2] gridPoints):
+        print(maxDistance)
         accumulatedResult = []
         for i in range(0,len(gridPoints)):
             result = self.getNeighborsInRangeForSingleQueryPoint(gridPoints[i],maxDistance)
             accumulatedResult.append(result)
         return accumulatedResult
 
-    def getNeighborsInRangeForSingleQueryPoint(self,np.ndarray[np.float64_t,ndim=1] qpoint, np.float64_t maxDistance)  :
+    cdef getNeighborsInRangeForSingleQueryPoint(self,np.ndarray[np.float64_t,ndim=1] qpoint, np.float64_t maxDistance) nogil :
         cdef deque[pair[double,Point]] deq
         cdef SphericalPoint spoint
         cdef Point point
@@ -83,7 +82,8 @@ cdef class PyVPTree:
         point = <Point>spoint
         point.setCoordinate1(qpoint[0])
         point.setCoordinate2(qpoint[1])
-        deq = self.vptree.getAllInRange(point,maxDistance)
+        with nogil:
+            deq = self.vptree.getAllInRange(point,maxDistance)
         cdef deque[pair[double,Point]].iterator it = deq.begin()
         result = []
         while it != deq.end():
